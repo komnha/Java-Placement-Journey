d7cd0e742d98cbc6474cf72833c65b09e06b9ec1
Komnhaaa_10X <130471321+komnha@users.noreply.github.com>
2026-07-15 00:21:46 +0530

    Create Addition.java

---
+++ 01_Basic/Addition.java
@@ -0,0 +1,9 @@
+class Addition{ 
+    public static void main (String[] args){
+        int a = 10;
+        int b = 30;
+        int c = a+b;
+        System.out.println("Sum:" + c);
+        
+    }
+}
