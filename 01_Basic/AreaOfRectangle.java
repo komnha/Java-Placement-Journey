790356012c10b16b0e7743e845da375170084b90
Komnhaaa_10X <130471321+komnha@users.noreply.github.com>
2026-07-15 00:23:00 +0530

    Create AreaOfRectangle.java

---
+++ 01_Basic/AreaOfRectangle.java
@@ -0,0 +1,9 @@
+class AreaOfRectangle{ 
+    public static void main (String[] args){
+       int length = 67;
+       int breadth = 67;
+       int area = length*breadth;
+    System.out.println("Area of rectangle: " + area);
+        
+    }
+}
