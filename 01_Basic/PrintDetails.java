3bd2d704f710a83d7e3c9f6594cef7beb532faf1
Komnhaaa_10X <130471321+komnha@users.noreply.github.com>
2026-07-15 00:19:53 +0530

    Create PrintDetails.java

---
+++ 01_Basic/PrintDetails.java
@@ -0,0 +1,7 @@
+class Details{ 
+    public static void main (String[] args){
+        System.out.println("Name: Komal");
+        System.out.println("Age:  23");
+        System.out.println("City: Dehradun");
+    }
+}
